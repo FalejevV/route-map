50b0f754dca3228dd8c55acde179e01da4a130f4
FalejevV <v.o.v.a@hotmail.com>
2023-06-28 18:37:57 +0300

    replace image on error test

--- src/components/CustomPopup/CustomPopup.tsx
+++ src/components/CustomPopup/CustomPopup.tsx
@@ -2,13 +2,18 @@ import { Pin } from "@/interface";
 import { Popup } from "react-leaflet";
 import Image from 'next/image'
 import ToolButton from "../ToolButton/ToolButton";
+import { useEffect, useRef, useState } from "react";
 
 
 export default function CustomPopup(props:{
     pin:Pin,
     onClick:Function
 }){
-    console.log(props.pin);
+    const [src, setSrc] = useState(props.pin.image);
+    function replaceImageOnError(){
+        setSrc('https://cdn-icons-png.flaticon.com/512/1079/1079536.png?w=1380&t=st=1687966598~exp=1687967198~hmac=a93e83f33b7863c387a4c188a8ef432bbfea4449bdcdb4b7e421048c32841d38')
+    }
+    
     return(
         <Popup>
             <div className="w-[250px] flex flex-col gap-3 p-2 pt-5 pb-7 relative">
@@ -16,7 +21,7 @@ export default function CustomPopup(props:{
                     <ToolButton icon={"clear.svg"} onClick={props.onClick} size={[20,20]} title={"remove"} />
                 </div>
                 <p>{props.pin.title}</p>
-                <Image src={props.pin.image} alt={"test image"} width={250} height={150} className="w-full h-[150px] object-cover bg-slate-200 rounded"/>
+                <Image src={src} onError={replaceImageOnError} alt={"pin image"} width={250} height={150} className="w-full h-[150px] object-cover bg-slate-200 rounded"/>
                 <p>{props.pin.description}</p>
             </div>
         </Popup>
